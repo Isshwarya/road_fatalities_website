ca6e3d5ba71fe63cdd6e55453055536d48a7f25e
Isshwarya Makudeswaran <isshwarya@Isshwaryas-MacBook-Pro.local>
2023-02-24 13:20:03 +0530

    Final changes before generating html pages

--- src/lib/load_charts.py
+++ src/lib/load_charts.py
@@ -1,16 +1,15 @@
-from lib.logger import DEBUG, INFO
-import lib.constants as Constants
-import lib.utils as Utils
 import os
 import re
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from jinja2 import Environment
 # import mpld3
 
-sns.set()
-sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
+from lib.logger import DEBUG, INFO
+import lib.constants as Constants
+import lib.utils as Utils
 
 
 class LoadCharts(object):
@@ -32,6 +31,10 @@ class LoadCharts(object):
               (self.start_year, self.end_year))
         assert min_yr <= self.start_year
         assert max_yr >= self.end_year
+        self.jinja_env = Environment()
+        sns.set()
+        sns.set_context("notebook", font_scale=1.5,
+                        rc={"lines.linewidth": 2.5})
 
     def run(self):
         INFO("Processing input data")
@@ -90,8 +93,8 @@ class LoadCharts(object):
         df = self.fatalities_df.groupby('state').size().sort_values()
 
         ax.set_xlim(0, df.index.size)
-        ax.set_yticks(np.arange(0, df.values.max(), Constants.STEP_SIZE))
-        ax.bar(df.index, df.values, align='center', width=0.5)
+        # ax.set_yticks(np.arange(0, df.values.max(), Constants.STEP_SIZE))
+        ax.bar(df.index, df.values, align='center')
 
         self._generate_chart(ax, fig, rotate_xlabels=True)
 
@@ -140,6 +143,8 @@ class LoadCharts(object):
         fig, (ax1, ax2) = plt.subplots(1, 2)
         ax1.set_title(
             'Change in Road Fatalities by %s between %s and %s' % (field.capitalize(), self.start_year, self.end_year))
+        ax1.set_title(self.start_year)
+        ax2.set_title(self.end_year)
         df = self.fatalities_df[self.fatalities_df['year'].isin(
             [self.start_year])].groupby(field).size()
         ax1.pie(df.values, labels=df.index, autopct='%1.1f%%',
@@ -161,7 +166,7 @@ class LoadCharts(object):
                autopct='%1.1f%%', textprops={"fontsize": 7})
         self._generate_chart(ax, fig)
 
-    def chart_facilities_by_age_compare(self):
+    def chart_fatalities_by_age_compare(self):
         fig, (ax1, ax2) = plt.subplots(1, 2)
         ax1.text(0.5, 1, self.start_year,
                  transform=ax1.transAxes, fontsize=10)
@@ -197,6 +202,19 @@ class LoadCharts(object):
         bins = range(0, self.fatalities_df["speed_limit"].max(
         )+(Constants.STEP_SIZE*2), Constants.STEP_SIZE)
         ax.hist(self.fatalities_df["age"], bins)
+        self._generate_chart(ax, fig)
+
+    def chart_fatalities_by_speed_limit_compare(self):
+        fig, ax = plt.subplots()
+        ax.set_title('Change in Road Fatalities by speed limit between %s and %s' % (
+            self.start_year, self.end_year))
+        ax.set_xlabel('Speed Limit')
+        ax.set_ylabel('Road Fatalities')
+        self.fatalities_df.loc[self.fatalities_df['year'] == self.start_year, 'speed_limit'].value_counts(
+        ).sort_index().plot(ax=ax, xlim=[10, 150], label=self.start_year)
+        self.fatalities_df.loc[self.fatalities_df['year'] == self.end_year, 'speed_limit'].value_counts(
+        ).sort_index().plot(ax=ax, xlim=[10, 150], label=self.end_year)
+        ax.legend()
         self._generate_chart(ax, fig)
 
     def chart_fatalities_by_weekday_compare(self):
@@ -250,3 +268,87 @@ class LoadCharts(object):
         # Eg: It changes year 1970 to 1,970, etc.,
 
         # mpld3.save_html(fig, os.path.join(self.charts_dir, caller + ".html"))
+
+    @staticmethod
+    def get_base_template():
+        return """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Australian Road Fatalities</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Home</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Impact of individual fields over the years
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item" href="auto/age_gender.html">Age and Gender</a></li>
+                  <li><a class="dropdown-item" href="auto/state.html">State </a></li>
+                  <li><a class="dropdown-item" href="auto/dayweek_hour_year.html">Dayweek, Hour and Year</a></li>
+                  <li><a class="dropdown-item" href="auto/road_user.html">Road User Role</a></li>
+                  <li><a class="dropdown-item" href="auto/crash_type.html">Crash Type</a></li>
+                  <li><a class="dropdown-item" href="auto/speed_limit.html">Speed Limit</a></li>
+                 </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Compare Statistics between two years
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="auto/compare/age_gender.html">Age and Gender</a></li>
+                    <li><a class="dropdown-item" href="auto/compare/state.html">State </a></li>
+                    <li><a class="dropdown-item" href="auto/compare/dayweek_hour_year.html">Dayweek, Hour and Year</a></li>
+                    <li><a class="dropdown-item" href="auto/compare/road_user.html">Road User</a></li>
+                    <li><a class="dropdown-item" href="auto/compare/crash_type.html">Crash Type</a></li>
+                    <li><a class="dropdown-item" href="auto/compare/speed_limit.html">Speed Limit</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+{content}
+</body>
+</html>
+"""
+
+    def generate_html_pages(self):
+        groups = [
+            ["age", "gender"],
+            ["state"],
+            ["dayweek", "hour", "year"],
+            ["road_user"],
+            ["crash_type"],
+            ["speed_limit"]
+        ]
+        for grp in groups:
+            self.generate_html_page(grp)
+
+    def generate_html_page(self, grp):
+        template = self.get_base_template()
+        content = """
+        {% for field in grp -%}
+        <h3>{{ field }}</h3>
+        <div>
+            # Individual statistics
+
+            # Compare Statistics
+        </div>
+"""
+
+        template.format(content=content)
+        jinja_template = self.jinja_env.from_string(template)
+        final_content = jinja_template.render()
